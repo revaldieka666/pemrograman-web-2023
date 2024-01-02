42e33bed60812495b0ac1c8366823eb6330ef409
revaldi eka setiawan <orangteknik8906@gmail.com>
2024-01-02 16:02:13 +0700

    first commit pake git bash

---
+++ praktikum8/ujicoba.js
@@ -0,0 +1,11 @@
+<script>
+            // output 1
+            window.alert("Selamat Pagi");
+            // output 2
+            document.write("STT Terpadu NF");
+            // output 3
+            let nama_kampus = "STTNF";
+            let alamat = "Jakarta Selatan";
+            console.log("Nama Kampus "+ nama_kampus);
+            console.log("Alamat " + alamat)
+        </script>
